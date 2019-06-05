61c3a962a2505f6d9d90349d137672ac6ed57103
admin <1234>
2019-06-05 22:54:05 +0700

    up

--- app/src/main/java/com/vn/khai/duan1khailqph05639/model/Danhsach.java
+++ app/src/main/java/com/vn/khai/duan1khailqph05639/model/Danhsach.java
@@ -3,95 +3,95 @@ package com.vn.khai.duan1khailqph05639.model;
 import java.util.Date;
 
 public class Danhsach {
- String id,chucvu,ten,gioitinh,tinhtrang,quoctich;
- int tuoi;
- double luong;
- Date date;
+    String id, chucvu, ten, gioitinh, tinhtrang, quoctich;
+    int tuoi;
+    double luong;
+    Date date;
 
- public Danhsach(String id, String chucvu, String ten, String gioitinh, String tinhtrang, String quoctich, int tuoi, double luong, Date date) {
-  this.id = id;
-  this.chucvu = chucvu;
-  this.ten = ten;
-  this.gioitinh = gioitinh;
-  this.tinhtrang = tinhtrang;
-  this.quoctich = quoctich;
-  this.tuoi = tuoi;
-  this.luong = luong;
-  this.date = date;
- }
+    public Danhsach(String id, String chucvu, String ten, String gioitinh, String tinhtrang, String quoctich, int tuoi, double luong, Date date) {
+        this.id = id;
+        this.chucvu = chucvu;
+        this.ten = ten;
+        this.gioitinh = gioitinh;
+        this.tinhtrang = tinhtrang;
+        this.quoctich = quoctich;
+        this.tuoi = tuoi;
+        this.luong = luong;
+        this.date = date;
+    }
 
- public Danhsach() {
- }
+    public Danhsach() {
+    }
 
- public String getId() {
-  return id;
- }
+    public String getId() {
+        return id;
+    }
 
- public void setId(String id) {
-  this.id = id;
- }
+    public void setId(String id) {
+        this.id = id;
+    }
 
- public String getChucvu() {
-  return chucvu;
- }
+    public String getChucvu() {
+        return chucvu;
+    }
 
- public void setChucvu(String chucvu) {
-  this.chucvu = chucvu;
- }
+    public void setChucvu(String chucvu) {
+        this.chucvu = chucvu;
+    }
 
- public String getTen() {
-  return ten;
- }
+    public String getTen() {
+        return ten;
+    }
 
- public void setTen(String ten) {
-  this.ten = ten;
- }
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
 
- public String getGioitinh() {
-  return gioitinh;
- }
+    public String getGioitinh() {
+        return gioitinh;
+    }
 
- public void setGioitinh(String gioitinh) {
-  this.gioitinh = gioitinh;
- }
+    public void setGioitinh(String gioitinh) {
+        this.gioitinh = gioitinh;
+    }
 
- public String getTinhtrang() {
-  return tinhtrang;
- }
+    public String getTinhtrang() {
+        return tinhtrang;
+    }
 
- public void setTinhtrang(String tinhtrang) {
-  this.tinhtrang = tinhtrang;
- }
+    public void setTinhtrang(String tinhtrang) {
+        this.tinhtrang = tinhtrang;
+    }
 
- public String getQuoctich() {
-  return quoctich;
- }
+    public String getQuoctich() {
+        return quoctich;
+    }
 
- public void setQuoctich(String quoctich) {
-  this.quoctich = quoctich;
- }
+    public void setQuoctich(String quoctich) {
+        this.quoctich = quoctich;
+    }
 
- public int getTuoi() {
-  return tuoi;
- }
+    public int getTuoi() {
+        return tuoi;
+    }
 
- public void setTuoi(int tuoi) {
-  this.tuoi = tuoi;
- }
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
 
- public double getLuong() {
-  return luong;
- }
+    public double getLuong() {
+        return luong;
+    }
 
- public void setLuong(double luong) {
-  this.luong = luong;
- }
+    public void setLuong(double luong) {
+        this.luong = luong;
+    }
 
- public Date getDate() {
-  return date;
- }
+    public Date getDate() {
+        return date;
+    }
 
- public void setDate(Date date) {
-  this.date = date;
- }
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
